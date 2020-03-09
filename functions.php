af7ac8cb01a7c5fd3ffa59d2610cc9837f24fbc8
Paco Marchante <paco@mowomo.com>
2020-03-09 17:31:00 +0100

    Refactorizando SCSS

--- functions.php
+++ functions.php
@@ -12,21 +12,3 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Añade todas las funciones que requiere el tema para funcionar.
  */
 require get_template_directory() . '/inc/class.origen.php';
-
-
-/*  ZONA DE WIDGET BARRA LATERAL
-=============================================== */
-
-origen_new_widget( 'Barra lateral' );
-origen_new_widget( 'Zona nueva 2' );
-
-origen_create_panel('prueba', 'Seccion de prueba', 'Esta es la descrioción');
-
-origen_create_section('misection', 'prueba');
-
-origen_create_setting('selector ejemplo', 'misection', 'select', array(
-	'full_width' => __( 'Pantalla completa' ),
-	'sidebar_r' => __( 'Barra lateral derecha' ))
-);
-
-origen_create_setting('el texto qui', 'misection', 'text', 'placeholder de ejemplo');
